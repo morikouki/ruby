4035f93f7fe2966a1ec64c388ffdc0549d89ed77
森考喜 <morikouki@morikoukinoMacBook-Pro.local>
2020-07-18 11:19:35 +0900

    [Finish]2-9

---
+++ definition.rb
@@ -0,0 +1,23 @@
+def hello_world
+	'Hello, World'
+end
+puts hello_world
+
+def greeting(name)
+	"Hello, #{name}!"
+end
+puts greeting('John')
+
+def greeting
+	return 'Hello, John!'
+	'Hi, John'
+end
+puts greeting
+
+puts 1 == 2
+
+puts 1 == 1
+
+puts 8 % 3
+
+puts 5 % 5
